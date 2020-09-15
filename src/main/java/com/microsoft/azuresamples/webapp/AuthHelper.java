ba739fc2f6276823ac900e06b692de9fe05c9eee
idg-sam <66391663+idg-sam@users.noreply.github.com>
2020-09-14 23:36:14 -0700

    updates

--- src/main/java/com/microsoft/azuresamples/webapp/AuthHelper.java
+++ src/main/java/com/microsoft/azuresamples/webapp/AuthHelper.java
@@ -1,49 +1,106 @@
 package com.microsoft.azuresamples.webapp;
 
 import com.microsoft.aad.msal4j.*;
+import com.microsoft.azuresamples.webapp.authentication.MsalAuthSession;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collections;
+import java.util.Date;
+import java.util.UUID;
+import java.util.concurrent.Future;
 
 import javax.servlet.http.HttpServletRequest;
-
-
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AuthHelper {
-    private ConfidentialClientApplication confClientInstance;
-    private String AUTHORITY = Config.getProperty("aad.authority");
-    private String CLIENT_ID = Config.getProperty("aad.clientId");
-    private String SECRET = Config.getProperty("aad.secret");
-    private String REDIRECT_URI = Config.getProperty("app.redirectUri");
+    private static ConfidentialClientApplication confClientInstance;
+    private static final String AUTHORITY = Config.getProperty("aad.authority");
+    private static final String CLIENT_ID = Config.getProperty("aad.clientId");
+    private static final String SECRET = Config.getProperty("aad.secret");
+    private static final String SCOPES = Config.getProperty("aad.scopes");
+    private static final Long STATE_TTL = Long.parseLong(Config.getProperty("app.stateTTL"));
+    private static final String REDIRECT_URI = Config.getProperty("app.redirectUri");
 
-    public ConfidentialClientApplication getConfidentialClientInstance() {
+    public static ConfidentialClientApplication getConfidentialClientInstance() {
         if (confClientInstance == null)
             return instantiateConfidentialClient();
         return confClientInstance;
     }
 
-    private String getAuthorizationRequestUrl(String state, String nonce) {
-        AuthorizationRequestUrlParameters parameters =
-                AuthorizationRequestUrlParameters
-                        .builder(REDIRECT_URI,
-                                Collections.singleton("openid offline_access profile"))
-                        .responseMode(ResponseMode.QUERY)
-                        .prompt(Prompt.SELECT_ACCOUNT)
-                        .state(state)
-                        .nonce(nonce)
-                        .build();
+    public static void doAuthorizationRequest(final HttpServletRequest req, final HttpServletResponse resp)
+            throws IOException {
+        final String state = UUID.randomUUID().toString();
+        final String nonce = UUID.randomUUID().toString();
+
+        final MsalAuthSession msalAuthSession = getMsalAuthSession(req.getSession());
+        msalAuthSession.setStateAndNonce(state, nonce);
+
+        resp.setStatus(302);
+        final String redirectUrl = getAuthorizationRequestUrl(SCOPES, state, nonce);
+        resp.sendRedirect(redirectUrl);
+    }
+
+    public static IAuthenticationResult processAuthCodeRedirect(final HttpServletRequest req) throws Exception {
+        final String nonce = req.getParameter("nonce");
+        final String state = req.getParameter("state");
+        final String authCode = req.getParameter("code");
+
+        if (authCode != null && validateNonceAndState(req.getSession(), nonce, state)) {
+            final AuthorizationCodeParameters authParams = AuthorizationCodeParameters.builder(
+                    authCode,
+                    new URI(req.getRequestURI())
+                    ).build();
+
+                    final Future<IAuthenticationResult> future = 
+                        AuthHelper.getConfidentialClientInstance().acquireToken(authParams);
+                    IAuthenticationResult result = future.get();
+                    if (result == null) {
+                        System.out.println("authentication result was null");
+                    }
+                    return result;
+        }
+        System.out.println("Couldn't process AuthCode");
+        return null;
+
+    }
+
+    private static boolean validateNonceAndState(final HttpSession session, final String nonce, final String state) {
+        final MsalAuthSession msalAuth = getMsalAuthSession(session);
+        final String savedState = msalAuth.getState();
+        final String savedNonce = msalAuth.getNonce();
+
+        final Date now = new Date();
+        if (savedNonce == null || savedState == null || nonce == null || state == null || !savedNonce.equals(nonce)
+                || !savedState.equals(state)
+                || msalAuth.getStateDate().before(new Date(now.getTime() - (STATE_TTL * 1000)))) {
+            System.out.println("Nonce/State mismatch or null or empty on validateNonceAndState");
+            return false;
+        }
+        return true;
+    }
+
+    public static MsalAuthSession getMsalAuthSession(final HttpSession session) {
+        return MsalAuthSession.getMsalAuthSession(session);
+    }
+
+    private static String getAuthorizationRequestUrl(final String scopes, final String state, final String nonce) {
+        final AuthorizationRequestUrlParameters parameters = AuthorizationRequestUrlParameters
+                .builder(REDIRECT_URI, Collections.singleton(scopes)).responseMode(ResponseMode.QUERY)
+                .prompt(Prompt.SELECT_ACCOUNT).state(state).nonce(nonce).build();
 
         return getConfidentialClientInstance().getAuthorizationRequestUrl(parameters).toString();
     }
 
-    private ConfidentialClientApplication instantiateConfidentialClient() {
+    private static ConfidentialClientApplication instantiateConfidentialClient() {
         try {
-            confClientInstance = ConfidentialClientApplication.builder(
-                    CLIENT_ID,
-                    ClientCredentialFactory.createFromSecret(SECRET))
-                    .b2cAuthority(AUTHORITY)
+            confClientInstance = ConfidentialClientApplication
+                    .builder(CLIENT_ID, ClientCredentialFactory.createFromSecret(SECRET)).b2cAuthority(AUTHORITY)
                     .build();
-        } catch (MalformedURLException ex) {
+        } catch (final MalformedURLException ex) {
             System.out.println("Failed to create Confidential Client Application");
         }
         return confClientInstance;
--- src/main/java/com/microsoft/azuresamples/webapp/authentication/MsalAuthSession.java
+++ src/main/java/com/microsoft/azuresamples/webapp/authentication/MsalAuthSession.java
@@ -1,12 +1,17 @@
 package com.microsoft.azuresamples.webapp.authentication;
+
 import javax.servlet.http.HttpSession;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.HashMap;
 
 public class MsalAuthSession implements Serializable {
     private static final long serialVersionUID = 1L;
     public static final String SESSION_KEY = "msalAuth";
+    private String nonce = null;
+    private String state = null;
+    private Date stateDate = null;
     private boolean authenticated = false;
     private String username = null;
     private HashMap<String,String> idTokenClaims = new HashMap<>();
@@ -20,6 +25,10 @@ public class MsalAuthSession implements Serializable {
         }
         
         return msalAuth;
+    }
+
+    public void saveMsalAuthSession(final HttpSession session) {
+        session.setAttribute(MsalAuthSession.SESSION_KEY, this);
     }
 
     public HashMap<String,String> getIdTokenClaims() {
@@ -52,5 +61,27 @@ public class MsalAuthSession implements Serializable {
 
     public void setAuthenticated(final boolean authenticated) {
         this.authenticated = authenticated;
+    }
+
+    public String getNonce() {
+        return this.nonce;
+    }
+
+    public void setNonce(String nonce) {
+        this.nonce = nonce;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public void setStateAndNonce(String state, String nonce) {
+        this.state = state;
+        this.nonce = nonce;
+        this.stateDate = new Date();
+    }
+
+    public Date getStateDate(){
+        return this.stateDate;
     }
 }
