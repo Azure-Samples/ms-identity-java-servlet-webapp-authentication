8cf030e3e4121b298c73f574d740efd6461fb07c
idg-sam <66391663+idg-sam@users.noreply.github.com>
2020-12-17 17:57:30 -0800

    update

---
+++ src/main/java/com/microsoft/azuresamples/authentication/HomePageServlet.java
@@ -0,0 +1,18 @@
+package com.microsoft.azuresamples.authentication;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HomePageServlet", urlPatterns = "/index")
+public class HomePageServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/auth_sign_in_status").forward(req, resp);
+    }
+
+}
