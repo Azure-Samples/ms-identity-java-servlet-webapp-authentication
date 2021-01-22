8bd19387b10a5225a06652fdbe1f5e67da24e9db
idg-sam <66391663+idg-sam@users.noreply.github.com>
2021-01-22 01:31:07 -0800

    overhaul of code and readme as per code review

--- src/main/java/com/microsoft/azuresamples/authentication/AuthenticationFilter.java
+++ src/main/java/com/microsoft/azuresamples/authentication/AuthenticationFilter.java
@@ -1,9 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.microsoft.azuresamples.authentication;
 
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -21,11 +23,6 @@ import javax.servlet.http.HttpServletResponse;
 public class AuthenticationFilter implements Filter {
 
     @Override
-    public void init(FilterConfig config) throws ServletException {
-        //
-    }
-
-    @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws ServletException, IOException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
@@ -40,10 +37,5 @@ public class AuthenticationFilter implements Filter {
         } else {
             chain.doFilter(req, res);
         }
-    }
-
-    @Override
-    public void destroy() {
-        //
     }
 }
---
+++ src/main/webapp/auth/graph.jsp
@@ -0,0 +1,10 @@
+<div class="card">
+    <h5 class="card-header bg-primary">
+        Call Graph /me Endpoint
+    </h5>
+    <div class="card-body">
+        <p class="card-text">
+            ${details}<br/>
+        </p>
+    </div>
+</div>
