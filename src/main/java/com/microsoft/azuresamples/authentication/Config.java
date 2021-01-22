561e25f414730da6714a005dcbe7376ade6f676a
idg-sam <66391663+idg-sam@users.noreply.github.com>
2021-01-21 18:39:14 -0800

    update readme and config

--- src/main/java/com/microsoft/azuresamples/authentication/Config.java
+++ src/main/java/com/microsoft/azuresamples/authentication/Config.java
@@ -18,12 +18,10 @@ public class Config implements ServletContextListener {
     public static Logger logger = Logger.getLogger("Logger");
     private static Properties props = Config.instantiateProperties();
 
-    public static final String REDIRECT = getProperty("app.redirectEndpoint");
-
     @Override
     public void contextInitialized(final ServletContextEvent event) {
         Config.logger.setLevel(Level.FINEST);
-        Config.logger.log(Level.INFO, "APPLICATION IS RUNNING ON http://SERVER-IP:PORT{0}/",
+        Config.logger.log(Level.INFO, "APPLICATION IS RUNNING ON http://SERVER-IP:PORT{0}/index",
                 event.getServletContext().getContextPath());
     }
 
